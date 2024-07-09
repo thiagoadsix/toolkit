21224c22cdd4d012e0223c32616e8681452cec4f
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-07-09 20:55:42 -0300

    test: add test for ReadJSON function

--- tools_test.go
+++ tools_test.go
@@ -1,6 +1,7 @@
 package toolkit
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"image/png"
@@ -225,5 +226,66 @@ func TestTools_DownloadStaticFile(t *testing.T) {
 	_, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+var readJsonTests = []struct {
+	name          string
+	json          string
+	errorExpected bool
+	maxSize       int
+	allowUnknown  bool
+}{
+	{name: "valid json", json: `{"foo": "bar"}`, errorExpected: false, maxSize: 1024, allowUnknown: false},
+	{name: "invalid json", json: `{"foo": "bar"`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "json too big", json: `{"foo": "bar"}`, errorExpected: true, maxSize: 1, allowUnknown: false},
+	{name: "json with unknown fields", json: `{"foo": "bar", "baz": "qux"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "json with unknown fields allowed", json: `{"foo": "bar", "baz": "qux"}`, errorExpected: false, maxSize: 1024, allowUnknown: true},
+	{name: "json with unknown fields disallowed", json: `{"foo": "bar", "baz": "qux"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "empty json", json: `{}`, errorExpected: false, maxSize: 1024, allowUnknown: false},
+	{name: "empty json with unknown fields", json: `{}`, errorExpected: false, maxSize: 1024, allowUnknown: true},
+	{name: "empty json with unknown fields disallowed", json: `{}`, errorExpected: false, maxSize: 1024, allowUnknown: false},
+	{name: "empty json with unknown fields allowed", json: `{}`, errorExpected: false, maxSize: 1024, allowUnknown: true},
+	{name: "json with incorrect type", json: `{"foo": 1}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "json duplicated", json: `{"foo": "bar"}{"foo": "bar"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "json missing field name", json: `{"": "bar"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "json is not json", json: `foo`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+}
+
+func TestTools_ReadJSON(t *testing.T) {
+	var testTools Tools
+
+	for _, e := range readJsonTests {
+		// set the max file size
+		testTools.MaxJSONSize = e.maxSize
+
+		// allow/disallow unknown fields
+		testTools.AllowUnknownFields = e.allowUnknown
+
+		// declare a variable to read the decoded JSON into
+		var decodedJSON struct {
+			Foo string `json:"foo"`
+		}
+
+		// create a new request with the JSON data
+		req, err := http.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(e.json)))
+		if err != nil {
+			t.Log("Error:", err)
+		}
+
+		// create a new response recorder
+		rr := httptest.NewRecorder()
+
+		err = testTools.ReadJSON(rr, req, &decodedJSON)
+
+		if e.errorExpected && err == nil {
+			t.Errorf("%s: expected error but none received", e.name)
+		}
+
+		if !e.errorExpected && err != nil {
+			t.Errorf("%s: error not expected, but one received: %s", e.name, err.Error())
+		}
+
+		req.Body.Close()
 	}
 }
